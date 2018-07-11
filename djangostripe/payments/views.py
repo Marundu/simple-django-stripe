b89d462ffe309c39c1117eccc3cbfd3d5d70a0de
marundu <marundu@gmail.com>
2018-07-11 17:40:46 +0300

    Added charging

--- djangostripe/payments/views.py
+++ djangostripe/payments/views.py
@@ -1,4 +1,25 @@
+import stripe
+
+from django.conf import settings
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
+
+stripe.api_key=settings.SECRET_KEY
 
 class HomePageView(TemplateView):
     template_name='home.html'
+    
+    def get_context_data(self, **kwargs): # new
+        context=super().get_context_data(**kwargs)
+        context['key']=settings.PUBLISHABLE_KEY
+        return context
+
+def charge(request): # new
+    if request.method=='POST': 
+        amount=500, 
+        currency='usd', 
+        description='A Django charge', 
+        source=request.POST['stripeToken'] 
+    )
+    
+    return render(request, 'charge.html')
